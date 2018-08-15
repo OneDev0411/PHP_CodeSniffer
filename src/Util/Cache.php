OneDev0411/PHP_CodeSniffer

commit 679f86c24ead49454dd51c706f12b8e12bf56a0f

Remove unused imports

src/Util/Cache.php
@@ -123,7 +123,7 @@ class Cache
             }
         );
 
-        $iterator  = new \RecursiveIteratorIterator($filter);
+        $iterator      = new \RecursiveIteratorIterator($filter);
         $codeHashFiles = [];
         foreach ($iterator as $file) {
             if (PHP_CODESNIFFER_VERBOSITY > 1) {

src/Standards/PSR12/Sniffs/Operators/OperatorSpacingSniff.php
@@ -9,7 +9,6 @@
 
 namespace PHP_CodeSniffer\Standards\PSR12\Sniffs\Operators;
 
-use PHP_CodeSniffer\Sniffs\Sniff;
 use PHP_CodeSniffer\Files\File;
 use PHP_CodeSniffer\Util\Tokens;
 use PHP_CodeSniffer\Standards\Squiz\Sniffs\WhiteSpace\OperatorSpacingSniff as SquizOperatorSpacingSniff;

src/Standards/Squiz/Sniffs/Scope/MemberVarScopeSniff.php
@@ -11,7 +11,6 @@ namespace PHP_CodeSniffer\Standards\Squiz\Sniffs\Scope;
 
 use PHP_CodeSniffer\Sniffs\AbstractVariableSniff;
 use PHP_CodeSniffer\Files\File;
-use PHP_CodeSniffer\Util\Tokens;
 
 class MemberVarScopeSniff extends AbstractVariableSniff
 {

commit 763d01ddadde36dc5ab7b03cedad9cfdf1139d40

Fixed a problem where the report cache was not being cleared when the sniffs inside a standard were updated (fixes issue introduced by #2081)

package.xml
@@ -26,6 +26,7 @@ http://pear.php.net/dtd/package-2.0.xsd">
  </stability>
  <license uri="https://github.com/squizlabs/PHP_CodeSniffer/blob/master/licence.txt">BSD 3-Clause License</license>
  <notes>
+  - Fixed a problem where the report cache was not being cleared when the sniffs inside a standard were updated
   - The info report (--report=info) now has improved formatting for metrics that span multiple lines
     -- Thanks to Juliette Reinders Folmer for the patch
   - The unit test runner now skips .bak files when looking for test cases

src/Util/Cache.php
@@ -123,8 +123,7 @@ class Cache
             }
         );
 
-        $iterator      = new \RecursiveIteratorIterator($filter);
-        $codeHashFiles = [];
+        $iterator = new \RecursiveIteratorIterator($filter);
         foreach ($iterator as $file) {
             if (PHP_CODESNIFFER_VERBOSITY > 1) {
                 echo "\t\t=> core file: $file".PHP_EOL;